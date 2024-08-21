0af0c83349644cbe44e534d29efad46fc3cf6ec8
Paolo Rampino <liadserv@gmail.com>
2024-08-21 05:02:00 -0400

    fo

---
+++ operazioni.py
@@ -0,0 +1,15 @@
+'''
+Addizione +
+Sottrazione -
+Moltiplicazione *
+Divisione /
+Divisione intera //
+Modulo %
+Potenza **
+'''
+
+print(13//4)
+print(13%4)
+print(f"La signora maestra ha detto che 13 diviso 4 da {13//4} con resto di {13%4}")
+print(13**5)
+print(f"Gente che non si fa fatti suoi {13**5}")
