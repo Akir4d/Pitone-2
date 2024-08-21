8177c5e1c11d9f58be13234a5b0ce1886da39c09
Paolo Rampino <liadserv@gmail.com>
2024-08-21 05:48:04 -0400

    up

--- condizioni.py
+++ condizioni.py
@@ -5,11 +5,11 @@
 > Maggiore di 
 >= Maggiore o uguale 
 != Diverso 
-Is/is not Identità dellʼoggetto / negazione 
-In / not in Eʼ contenuto / non è contenuto 
-And AND logico 
-Or OR logico 
-Not NOT logico
+is/is not Identità dell'oggetto / negazione 
+in/not in E' contenuto / non è contenuto 
+and AND logico 
+or OR logico 
+not NOT logico
 """
 x = 10
 y = int(input("Inserisci y: "))
@@ -18,3 +18,9 @@ if x < y:
     print("x e' minore di y")
 else:
     print("x e' maggiore o uguale a y")
+frutta = ["banana", "pera", "pesca"]
+chefrutta = input("frutta: ")
+if chefrutta.lower() in frutta:
+    print(f"Abbiamo {chefrutta}")
+else:
+    print(f"Non abbiamo {chefrutta}")
